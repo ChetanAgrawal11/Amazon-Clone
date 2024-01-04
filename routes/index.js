fc524ee5d14d21b9322de1355147ff4d8c02e76b
ChetanAgrawal11 <chetanagrawal117@gmail.com>
2024-01-04 19:46:48 +0530

    SignIn and Login page created

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get("/", function (req, res, next) {
+  res.render("index");
 });
-router.get('/register', function(req, res, next) {
-  res.render('register');
+router.get("/login", function (req, res, next) {
+  res.render("login");
 });
-
-
+router.get("/register", function (req, res) {
+  res.render("register");
+});
 
 module.exports = router;
